fcfa07e6b8ce59fe707bafa4967138d70398a83c
Bree <barmstrong@arccorp.com>
2023-10-05 15:10:19 -0400

    update intern page to match pdf

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import {
   Stickynav,
   ProductJumbo,
   ProductText,
+  ProductCallout,
   ProductLinks,
 } from "arccorp-vars";
 
@@ -47,11 +48,9 @@ export default function Internships() {
             <div className="col-lg-6">
               <div className="row">
                 <div className="inner-column">
-                  <div className="internship-details-title">Dates</div>
+                  <div className="internship-details-title">DATES</div>
                   <div className="internship-details-copy">
-                    <span className="intern-dates">
-                      June 5 – August 11, 2023
-                    </span>
+                    <span className="intern-dates">June – August 2024</span>
                   </div>
                 </div>
                 <div className="inner-column">
@@ -66,7 +65,7 @@ export default function Internships() {
                     Louisville, Kentucky or Tampa, Florida areas based on ARC’s
                     headquarters. However, roles are open to any applicants in
                     the U.S. The internship will be mostly virtual and remote,
-                    with potential opportunities to come into the office.
+                    with opportunities to come into the office
                   </p>
                 </div>
               </div>
@@ -87,7 +86,7 @@ export default function Internships() {
                       <span className="copy-focus-pay">$25</span>
                       <span className="copy-focus-text">/hour</span>
                       <div className="copy-uppercase">
-                        For Graduate Internships
+                        For Graduate STUDENT Internships
                       </div>
                     </div>
                   </div>
@@ -96,9 +95,8 @@ export default function Internships() {
                   <div className="internship-details-title">Requirements</div>
                   <p className="internship-details-copy">
                     You must be a current student and will need to submit an
-                    unofficial transcript. ARC requires all employees, including
-                    interns, to be vaccinated against COVID-19. We will consider
-                    medical and religious accommodations as required by law.
+                    unofficial transcript. We will consider medical and
+                    religious accommodations as required by law.
                   </p>
                 </div>
               </div>
@@ -116,7 +114,9 @@ export default function Internships() {
           <div class="container">
             <div class="row">
               <div class="col-lg-12">
-                <h2 class="product-header white intern-title">Internship Benefits</h2>
+                <h2 class="product-header white intern-title">
+                  Internship Benefits
+                </h2>
               </div>
             </div>
           </div>
@@ -238,213 +238,67 @@ export default function Internships() {
       </div>
 
       {/* Available Internships */}
-      {/* <ProductText
+      <ProductText
         id="internships"
-        title="Available Internships"
+        subtitle="Available interships will be posted Spring 2024, here are a list of departments that will have internships:"
         className="available-internships intern-title"
         body={
           <div className="row">
-            <div className="col-lg-6">
+            <div className="col-lg-3">
               <div className="position-inner">
-                <a
-                  className="position-link"
-                  href="https://careers-arccorp.icims.com/jobs/2264/data-engineering-intern---summer-2023/job"
-                >
-                  <div className="position-title">
-                    Data Engineering Intern (2)
-                  </div>
-                </a>
+                <div className="position-title">Business Technology</div>
               </div>
             </div>
-            <div className="col-lg-6">
-              <div className="position-inner left-position">
-                <a
-                  className="position-link"
-                  href="https://careers-arccorp.icims.com/jobs/2257/software-engineering-intern---summer-2023/job"
-                >
-                  <div className="position-title">
-                    Software Engineering Intern (2)
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-6">
+            <div className="col-lg-3">
               <div className="position-inner">
-                <a
-                  className="position-link"
-                  href="https://careers-arccorp.icims.com/jobs/2256/multimedia-and-graphic-design-intern--summer-2023/job"
-                >
-                  <div className="position-title">
-                    Multimedia and Graphic Design Intern (1)
-                  </div>
-                </a>
+                <div className="position-title">Marketing</div>
               </div>
             </div>
-            <div className="col-lg-6">
-              <div className="position-inner left-position">
-                <a
-                  className="position-link"
-                  href="https://careers-arccorp.icims.com/jobs/2262/enterprise-risk-intern--summer-2023/job"
-                >
-                  <div className="position-title">
-                    Enterprise Risk Intern (1)
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-6">
+            <div className="col-lg-3">
               <div className="position-inner">
-                <a
-                  className="position-link"
-                  href="https://careers-arccorp.icims.com/jobs/2255/platform-engineering-intern---summer-2023/job"
-                >
-                  <div className="position-title">
-                    Platform Engineering Intern (1)
-                  </div>
-                </a>
+                <div className="position-title">Finance and Accounting</div>
               </div>
             </div>
-            <div className="col-lg-6">
-              <div className="position-inner left-position">
-                <a
-                  className="position-link"
-                  href="https://careers-arccorp.icims.com/jobs/2261/product-analyst-intern--summer-2023/job"
-                >
-                  <div className="position-title">
-                    Product Analyst Intern (1)
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-6">
+            <div className="col-lg-3">
               <div className="position-inner">
-                <a
-                  className="position-link"
-                  href="https://careers-arccorp.icims.com/jobs/2254/accounting-intern--summer-2023/job"
-                >
-                  <div className="position-title">Accounting Intern (1)</div>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-6">
-              <div className="position-inner left-position">
-                <a
-                  className="position-link"
-                  href="https://careers-arccorp.icims.com/jobs/2260/software-quality-engineer-intern---summer-2023/job"
-                >
-                  <div className="position-title">
-                    Software Quality Engineer Intern (2)
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-6">
-              <div className="position-inner">
-                <a
-                  className="position-link"
-                  href="https://careers-arccorp.icims.com/jobs/2253/financial-planning-%26-analysis-%28fp%26a%29-intern--summer-2023/job"
-                >
-                  <div className="position-title">FP&A Intern (1)</div>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-6">
-              <div className="position-inner left-position">
-                <a
-                  className="position-link"
-                  href="https://careers-arccorp.icims.com/jobs/2259/data-analyst-intern--summer-2023/job"
-                >
-                  <div className="position-title">Data Analyst Intern (1)</div>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-6">
-              <div className="position-inner left-position">
-                <a
-                  className="position-link"
-                  href="https://careers-arccorp.icims.com/jobs/2252/salesforce-intern--summer-2023/job"
-                >
-                  <div className="position-title">Salesforce Intern (1)</div>
-                </a>
+                <div className="position-title">Business Operations</div>
               </div>
             </div>
           </div>
         }
-      /> */}
-      {/* Resources */}
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div
-              className="arc-homepage-card lazy"
-              data-bg={
-                "https://www2.arccorp.com/globalassets/homepage/redesign/internship/resources.png"
-              }
-              style={{ marginTop: "90px" }}
-            >
-              <div className="row align-items-center no-gutters">
-                <div
-                  className="col-lg-12"
-                  style={{ minHeight: "auto", height: "auto" }}
-                >
-                  <div className="arc-homepage-data d-flex flex-column">
-                    <div className="arc-homepage-data-title">Resources</div>
-                    <div>
-                      <div>
-                        <p>
-                          <strong className="interview-title">
-                            The interview process will include:
-                          </strong>
-                        </p>
-                        <li className="interview-step">
-                          A telephone screening with a recruiter from ARC
-                        </li>
-                        <li className="interview-step">
-                          Interviews with your hiring manager and other ARC
-                          colleagues
-                        </li>
-                      </div>
-                      <div style={{ paddingTop: "30px" }}>
-                        <p>
-                          <strong className="interview-title">
-                            For More Information
-                          </strong>
-                        </p>
-                        <p className="interview-step">
-                          Contact our Talent Acquisition team (
-                          <a href="mailto:jobs@arccorp.com">jobs@arccorp.com</a>
-                          ).
-                        </p>
-                      </div>
-                    </div>
-                    <div></div>
-                  </div>
-                </div>
+      />
+      <ProductText
+      className="internship-info"
+        body={
+          <>
+            <div className="row ">
+              <div className="col-lg-6">
+                <div className="info-about-title">Interview Process</div>
+                <div className="info-copy">The interview process will include:</div>
+                <ul>
+                  <li><span className="list">A telephone screening with a recruiter from ARC</span></li>
+                  <li>Interviews with your hiring manager and other ARC colleagues</li>
+                </ul>
+              </div>
+              <div className="col-lg-6">
+              <div className="info-about-title">For More Information</div>
+                <div className="info-copy"><a href="mailto:jobs@arccorp.com">Contact</a> our Talent Acquisition team</div>
+                <div className="internship-details-title">Resources</div>
+                <a className="info-link" href="https://careers-arccorp.icims.com/jobs/search?hashed=-435595431&mobile=false&width=960&height=500&bga=true&needsRedirect=false&jan1offset=-300&jun1offset=-240">Explore full-time opportunities</a>
+                <br/>
+                <a className="info-link" href="https://www2.arccorp.com/about-us/careers/our-culture/?utm_source=internship">Learn more about our culture</a>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <ProductLinks
-      className="internship-resources"
-        prodLink={[
-          {
-            alt: "Learn more about our culture",
-            cta: "Learn More",
-            copy: "At ARC, we believe work should be rewarding and meaningful. We collaborate, think big and connect around shared experiences.",
-            header: "Learn more about our culture",
-            icon: "https://www2.arccorp.com/globalassets/homepage/redesign/internship/culture.png",
-            link: "https://www2.arccorp.com/about-us/careers/our-culture/",
-          },
-          {
-            alt: "Explore full-time opportunities",
-            header: "Explore full-time opportunities",
-            copy: "With career development opportunities and comprehensive benefits, we help our employees thrive — at home and at work.",
-            cta: "Learn More",
-            icon: "https://www2.arccorp.com/globalassets/homepage/redesign/internship/opportunities.png",
-            link: "https://careers-arccorp.icims.com/jobs/search",
-          },
-        ]}
+          </>
+        }
+      />
+
+      {/* About ARC */}
+      <ProductCallout
+        className="internships-about"
+        title="About ARC"
+        subtitle="ARC accelerates the growth of global air travel by delivering forward-looking travel data, flexible distribution services and innovative industry solutions. We are a leading travel intelligence company that possesses the world’s largest, most comprehensive global airline ticket data, including more than 15 billion passenger flights representing 490 airlines
+and 230 countries and territories. Our technology, products and expertise strengthen economies and enrich lives by connecting stakeholders across the travel ecosystem."
       />
     </div>
   );
